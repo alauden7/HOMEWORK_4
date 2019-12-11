6cbef0c9a371e64fd3a9477cf3d9810037c4aa40
Anya Laudenslager <anyalauden@gmail.com>
2019-12-10 23:22:20 -0500

    Homework4

---
+++ questions.js
@@ -0,0 +1,30 @@
+var myQuestions = [
+  {
+    question: "What is the HTML tag under which one can write the JavaScript code?",
+    answers: {
+      a: "<scripted>",
+      b: "<js>",
+      c: "<script>"
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "Javascript is a ___ language.",
+    answers: {
+      a: "programming",
+      b: "scripting",
+      c: "application"
+    },
+    correctAnswer: "b"
+  },
+  {
+    question: "Where is Waldo really?",
+    answers: {
+      a: "Antarctica",
+      b: "Exploring the Pacific Ocean",
+      c: "Sitting in a tree",
+      d: "Minding his own business, so stop asking"
+    },
+    correctAnswer: "d"
+  }
+];
